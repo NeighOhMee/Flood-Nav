6e4ae2283cfa170fabad56e64b3a3d5fdc491f37
TheNamor <thenamor2@gmail.com>
2019-11-02 14:56:30 -0400

    ui pygames change to grid

--- src/ui.py
+++ src/ui.py
@@ -6,7 +6,9 @@ This module controls the pygames output interface
 
 import pygame
 
-def startUp():
+def startUp(dimensions, grid):
+    dimensions = dimensions
+    board = grid
     done = True
 
 def shutDown():
@@ -16,21 +18,11 @@ def drawRect(screen, color, position, size):
     pygame.draw.rect(screen, color, pygame.Rect(position[0], position[1], size[0], size[1]))
 
 pygame.init()
+dimensions = (3, 3)
 myfont = pygame.font.SysFont('bahnschrift', 50)
-screen = pygame.display.set_mode((1485, 685))
+screen = pygame.display.set_mode(((dimensions[0]-1)*40+5, (dimensions[1]-1)*40+5))
 done = False
 clock = pygame.time.Clock()
-board = []
-
-for i in range(35):
-        if i % 2 == 0:
-            board.append([0]*37)
-        else:
-            board.append([0]*38)
-
-board[0][0] = 1
-board[1][1] = 2
-board[15][20] = 1
 
 while not done:
     for event in pygame.event.get():
@@ -38,23 +30,10 @@ while not done:
                     done = True
     screen.fill((194, 210, 180))
     pressed = pygame.key.get_pressed()
-    for i in range(38):
-        for j in range(18):
+    for i in range(dimensions[0]):
+        for j in range(dimensions[1]):
             drawRect(screen, (0, 0, 0), (i*40, j*40), (5, 5))
-    for i in range(35):
-        if i % 2 == 0:
-            j_range = 37
-        else:
-            j_range = 38
-        for j in range(j_range):
-            if board[i][j] == 1:
-                color = (0, 153, 255)
-            elif board[i][j] == 2:
-                color = (255, 0, 0)
-            else:
-                color = (0, 255, 0)
-            if i % 2 == 0: drawRect(screen, color, ((j)*40+5, (i//2)*40+1), (35, 3))
-            if i % 2 == 1: drawRect(screen, color, ((j)*40+1, (i//2)*40+5), (3, 35))
+    
     clock.tick()
     pygame.display.flip()    
 pygame.quit()
