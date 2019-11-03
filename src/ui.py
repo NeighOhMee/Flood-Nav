64971f2f65f4cbd04604b969933af6b00cfbc152
TheNamor <thenamor2@gmail.com>
2019-11-02 20:15:02 -0400

    ui change

--- src/ui.py
+++ src/ui.py
@@ -5,6 +5,7 @@ This module controls the pygames output interface
 '''
 
 import pygame
+import grid
 
 def startUp(dimensions, grid):
     dimensions = dimensions
@@ -18,7 +19,11 @@ def drawRect(screen, color, position, size):
     pygame.draw.rect(screen, color, pygame.Rect(position[0], position[1], size[0], size[1]))
 
 pygame.init()
-dimensions = (3, 3)
+dimensions = (32, 32)
+board = grid.grid([(1,0,2,0),(1,0,1,1),(1,1,2,1),(0,2,1,2)], (0,0), (32,17))
+board.addFlooded((2, 0), (1, 1))
+board.addFlooded((0, 2), (1, 1))
+board.addFlooded((2, 2), (1, 1))
 myfont = pygame.font.SysFont('bahnschrift', 50)
 screen = pygame.display.set_mode(((dimensions[0]-1)*40+5, (dimensions[1]-1)*40+5))
 done = False
@@ -32,8 +37,23 @@ while not done:
     pressed = pygame.key.get_pressed()
     for i in range(dimensions[0]):
         for j in range(dimensions[1]):
+            for direction in [(0, 1), (1, 0), (1, 1)]:
+                connection_status = board.checkConnection((i, j), (i+direction[0], j+direction[1]))
+                if connection_status < 2:
+                    if connection_status == 0:
+                        color = (0, 255, 0)
+                    else:
+                        color = (0, 153, 255)
+                    if direction[0] and not direction[1]:
+                        drawRect(screen, color, (j*40+1, i*40+5), (3, 40))
+                    elif not direction[0] and direction[1]:
+                        drawRect(screen, color, (j*40+5, i*40+1), (40, 3))
+                    else:
+                        pygame.draw.polygon(screen, color, [(j+5, i+4), (j+4, i+5), (j+45), (i+45)], 0)
+
+    for i in range(dimensions[0]):
+        for j in range(dimensions[1]):
             drawRect(screen, (0, 0, 0), (i*40, j*40), (5, 5))
-    
     clock.tick()
     pygame.display.flip()    
 pygame.quit()
